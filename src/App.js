1f79bb10544c13d765c85adf4c3ddd97b19a85c1
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-08-19 19:27:22 -0600

    Added Job Postings

--- src/App.js
+++ src/App.js
@@ -2,31 +2,21 @@ import React,{useState,useEffect} from 'react';
 import APIRequests from './services/APIRequest';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import LinkList from './components/LinkList';
-import Summary from './components/Summary';
+import JobPostings from './components/JobPostings'
 
 function App() {
-  const [jobLinks, changeJobLinks] = useState([{loading:true}]);
+  const [jobPostings, changeJobPostings] = useState([]);
   
   useEffect(() => {
     APIRequests.getJobLinks().then(links => {
-      changeJobLinks(links)
+      changeJobPostings(links)
     })
   }, [])
 
-  const liveLinks = jobLinks.filter(link => link.deadLink === false);
-  const deadLinks = jobLinks.filter(link => link.deadLink === true);
-  const totalJobs = jobLinks.length;
 
   return (
     <div className="App container">
-        <Summary totalJobs = {totalJobs} />
-      <div>
-        <LinkList Links ={liveLinks} linkType ="Live" />
-      </div>
-      <div>
-        <LinkList Links = {deadLinks} linkType = "Dead" />
-      </div>
+      <JobPostings jobPostings = {jobPostings}/>
     </div>
   );
 
---
+++ src/components/JobPostings.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import LinkList from './LinkList';
+import Summary from './Summary';
+
+const JobPostings = ({jobPostings}) =>{
+    
+  const liveLinks = jobPostings.filter(link => link.deadLink === false);
+  const deadLinks = jobPostings.filter(link => link.deadLink === true);
+  const totalJobs = jobPostings.length;
+    
+    return (
+        <>
+            <Summary totalJobs={totalJobs} />
+            <div>
+                <LinkList Links={liveLinks} linkType="Live" />
+            </div>
+            <div>
+                <LinkList Links={deadLinks} linkType="Dead" />
+            </div>
+        </>
+    )
+}
+
+export default JobPostings;
